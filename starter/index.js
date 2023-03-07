2312483bef80e05afeb52307aec5188d8944d45c
Lola <86410482+19Lola89@users.noreply.github.com>
2023-03-07 17:29:07 +0000

    added prompts

--- starter/index.js
+++ starter/index.js
@@ -12,214 +12,278 @@ const render = require("./src/page-template.js");
 
 const team = [];
 
-const managerQuestions = [
-  //team manager questions
-  {
-    type: "input",
-    name: "name",
-    message: "What is your name? ",
-    validate: (input) => {
-      if (!input) {
-        return "Please enter correct name";
-      }
-      return true;
-    },
-  },
-  {
-    type: "input",
-    name: "employeeID",
-    message: "What is your employee ID?",
-    validate: (input) => {
-      if (!input) {
-        return "Please enter your employee ID";
-      }
-      return true;
-    },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-    validate: function (input) {
-      // Regular expression to match email address format
-      const emailRegex = /\S+@\S+\.\S+/;
+const managerQuestions = () => {
+  return inquirer
+    .prompt([
+      //team manager questions
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name? ",
+        validate: (input) => {
+          if (!input) {
+            return "Please enter correct name";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "employeeID",
+        message: "What is your employee ID?",
+        validate: (input) => {
+          if (!input) {
+            return "Please enter your employee ID";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+        validate: function (input) {
+          // Regular expression to match email address format
+          const emailRegex = /\S+@\S+\.\S+/;
 
-      if (emailRegex.test(input)) {
-        return true;
-      } else {
-        return "Please enter a valid email address";
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "office",
-    message: "What is your office number?",
-    validate: (input) => {
-      if (!input) {
-        return "Please provide an office number";
-      }
-      return true;
-    },
-  },
-];
+          if (emailRegex.test(input)) {
+            return true;
+          } else {
+            return "Please enter a valid email address";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office number?",
+        validate: (input) => {
+          if (!input) {
+            return "Please provide an office number";
+          }
+          return true;
+        },
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.employeeID,
+        answers.email,
+        answers.officeNumber
+      );
+      team.push(manager);
+      askQuestions();
+    });
+};
 
 // engineer questions
 
-const engineerQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is your name? ",
-    validate: (input) => {
-      if (!input) {
-        return "Please enter correct name";
-      }
-      return true;
-    },
-  },
-  {
-    type: "input",
-    name: "employeeID",
-    message: "What is your employee ID?",
-    validate: (input) => {
-      if (!input) {
-        return "Please enter your employee ID";
-      }
-      return true;
-    },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-    validate: function (input) {
-      // Regular expression to match email address format
-      const emailRegex = /\S+@\S+\.\S+/;
+const engineerQuestions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name? ",
+        validate: (input) => {
+          if (!input) {
+            return "Please enter correct name";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "employeeID",
+        message: "What is your employee ID?",
+        validate: (input) => {
+          if (!input) {
+            return "Please enter your employee ID";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+        validate: function (input) {
+          // Regular expression to match email address format
+          const emailRegex = /\S+@\S+\.\S+/;
 
-      if (emailRegex.test(input)) {
-        return true;
-      } else {
-        return "Please enter a valid email address";
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "Please add your github username",
-    validate: (input) => {
-      if (!input) {
-        return "Please enter correct username";
-      }
-      return true;
-    },
-  },
-];
+          if (emailRegex.test(input)) {
+            return true;
+          } else {
+            return "Please enter a valid email address";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Please add your github username",
+        validate: (input) => {
+          if (!input) {
+            return "Please enter correct username";
+          }
+          return true;
+        },
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.name,
+        answers.employeeID,
+        answers.email,
+        answers.gitHub
+      );
+      team.push(engineer);
+      askQuestions();
+    });
+};
 
 // intern questions
 
-const internQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is your name? ",
-    validate: (input) => {
-      if (!input) {
-        return "Please enter correct name";
-      }
-      return true;
-    },
-  },
-  {
-    type: "input",
-    name: "employeeID",
-    message: "What is your employee ID?",
-    validate: (input) => {
-      if (!input) {
-        return "Please enter your employee ID";
-      }
-      return true;
-    },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-    validate: function (input) {
-      // Regular expression to match email address format
-      const emailRegex = /\S+@\S+\.\S+/;
+const internQuestions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name? ",
+        validate: (input) => {
+          if (!input) {
+            return "Please enter correct name";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "employeeID",
+        message: "What is your employee ID?",
+        validate: (input) => {
+          if (!input) {
+            return "Please enter your employee ID";
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+        validate: function (input) {
+          // Regular expression to match email address format
+          const emailRegex = /\S+@\S+\.\S+/;
 
-      if (emailRegex.test(input)) {
-        return true;
-      } else {
-        return "Please enter a valid email address";
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "Please add your school name",
-    validate: (input) => {
-      if (!input) {
-        return "Please enter correct school";
-      }
-      return true;
-    },
-  },
-];
-
+          if (emailRegex.test(input)) {
+            return true;
+          } else {
+            return "Please enter a valid email address";
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Please add your school name",
+        validate: (input) => {
+          if (!input) {
+            return "Please enter correct school";
+          }
+          return true;
+        },
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.employeeID,
+        answers.email,
+        answers.school
+      );
+      team.push(intern);
+      askQuestions();
+    });
+};
 // prompt for choosing which set of questions to answer
 
-inquirer.prompt([
-  {
-    type: "list",
-    name: "questionChoise",
-    message: "Please choose your position",
-    choices: ["manager", "engineer", "intern"],
-  },
-]);
+const askQuestions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "questionSet",
+        message: "Please choose your position",
+        choices: ["manager", "intern", "engineer", "done"],
+      },
+    ])
+    .then((userChoice) => {
+      switch (userChoice.questionSet) {
+        case "engineer":
+          engineerQuestions();
+          break;
 
-if (team.some((employee) => employee.getRole() === "Manager")) {
-  console.log("A manager has already been added to the team!");
-  return addEmployee();
+        case "intern":
+          internQuestions();
+          break;
+
+        case "manager":
+          managerQuestions();
+          break;
+
+        default:
+          buildTeam();
+      }
+    });
+};
+askQuestions();
+
+// team builder
+const buildTeam = () => {
+  console.log("The team has been built");
+};
+
+if (!fs.existsSync(OUTPUT_DIR)) {
+  fs.mkdirSync(OUTPUT_DIR);
 }
+fs.writeFileSync(outputPath, render(team), "utf-8");
+//------------------------------------------------------------------
 
-const addManager = async () => {
-  const answers = await inquirer.prompt(managerQuestions);
-  const manager = new Manager(
-    answers.name,
-    answers.id,
-    answers.email,
-    answers.officeNumber
-  );
-  team.push(manager);
-  addEmployee();
-};
+// const addManager = async () => {
+//   const answers = await inquirer.prompt(managerQuestions);
+//   const manager = new Manager(
+//     answers.name,
+//     answers.id,
+//     answers.email,
+//     answers.officeNumber
+//   );
+//   team.push(manager);
+//   addEmployee();
+// };
 
-const addEngineer = async () => {
-  const answers = await inquirer.prompt(engineerQuestions);
-  const engineer = new Engineer(
-    answers.name,
-    answers.id,
-    answers.email,
-    answers.github
-  );
-  team.push(engineer);
-  addEmployee();
-};
+// const addEngineer = async () => {
+//   const answers = await inquirer.prompt(engineerQuestions);
+//   const engineer = new Engineer(
+//     answers.name,
+//     answers.id,
+//     answers.email,
+//     answers.github
+//   );
+//   team.push(engineer);
+//   addEmployee();
+// };
 
-const addIntern = async () => {
-  const answers = await inquirer.prompt(internQuestions);
-  const intern = new Intern(
-    answers.name,
-    answers.id,
-    answers.email,
-    answers.school
-  );
-  team.push(intern);
-  addEmployee();
-};
-
-// add then and when
+// const addIntern = async () => {
+//   const answers = await inquirer.prompt(internQuestions);
+//   const intern = new Intern(
+//     answers.name,
+//     answers.id,
+//     answers.email,
+//     answers.school
+//   );
+//   team.push(intern);
+//   addEmployee();
+// };
